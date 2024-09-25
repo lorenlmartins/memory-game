f2e3f990228f9892aef2739afba544baf018cf7e
Loren Lima Martins <loren.lima@take.net>
2024-09-25 13:56:27 -0300

    feat: create rotate effect

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -19,9 +19,21 @@ let openCards = [];
 
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
-for(let i=0; i<emojis.length; i++){
+for (let i = 0; i < emojis.length; i++) {
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
-    document.querySelector(".game").appendChild(box);
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box)
 }
+function handleClick() {
+    if(openCards.length < 2) {
+        this.classList.add('boxOpen');
+        openCards.push(this);
+    }
+    if(openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+ }
+ function checkMatch() {
+ }
